5109896a8fa51037e0fe2fb84c39ffda468d7f47
M.Navid Afzali <89129279+NavidAfzali@users.noreply.github.com>
2022-12-25 22:21:47 +0330

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             UserSeeder::class,
-            CouponSeeder::class,
+            //CouponSeeder::class,
         ]);
     }
 }
