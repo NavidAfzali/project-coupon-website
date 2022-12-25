a6b849b0bcf6f4555b1aef6a100a9efc51f2fca7
M.Navid Afzali <89129279+NavidAfzali@users.noreply.github.com>
2022-12-25 22:15:01 +0330

    Update today-suggest.blade.php

--- resources/views/today-suggest.blade.php
+++ resources/views/today-suggest.blade.php
@@ -1,14 +1,14 @@
 <p class="mt-5 text-center head">Shop Today's Trending Deals and Save Big</p>
-@if(count($coupons)>0)
+@if(count($coupons)>3)
 <div class="container col-expand-lg text-center mt-5">
 <div class="row">
 <div class="col">
         <div class="card text-center text-bg-light cd">
-            <img src="{{ asset('/storage/' . $coupons[3]->image) }}" class="card-img haraji" alt="tools">
-            <a href="/coupon/{{ $coupons[3]->id }}" class=" stretched-link"></a>
+            <img src="{{ asset('/storage/' . $coupons[1]->image) }}" class="card-img haraji" alt="tools">
+            <a href="/coupon/{{ $coupons[1]->id }}" class=" stretched-link"></a>
             <div class="card-img-overlay">
                 <div class="a2">
-                    <h4 class="card-title" style="margin-left: 15px;">{{ $coupons[3]->name }}</h4>
+                    <h4 class="card-title" style="margin-left: 15px;">{{ $coupons[1]->name }}</h4>
                     <p class="card-text" style="margin-left: 15px;"><small>Last updated 7 mins ago</small></p>
                 </div>
             </div>
@@ -16,11 +16,11 @@
     </div>
     <div class="col">
         <div class="card text-center text-bg-light cd">
-            <img src="{{ asset('/storage/' . $coupons[5]->image) }}" class="card-img haraji" alt="tools">
-            <a href="/coupon/{{ $coupons[5]->id }}" class=" stretched-link"></a>
+            <img src="{{ asset('/storage/' . $coupons[2]->image) }}" class="card-img haraji" alt="tools">
+            <a href="/coupon/{{ $coupons[2]->id }}" class=" stretched-link"></a>
             <div class="card-img-overlay">
                 <div class="a2">
-                    <h4 class="card-title" style="margin-left: 15px;">{{ $coupons[5]->name }}</h4>
+                    <h4 class="card-title" style="margin-left: 15px;">{{ $coupons[2]->name }}</h4>
                     <p class="card-text" style="margin-left: 15px;"><small>Last updated 7 mins ago</small></p>
                 </div>
                 
